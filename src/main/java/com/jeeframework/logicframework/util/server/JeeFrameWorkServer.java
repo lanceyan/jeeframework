b1ca63b4358024dcd27bb9c620e69ceae3bdf84b
lance <lance@weicitech.com>
2017-01-17 10:46:28 +0800

    添加 -Dlog.dir 默认配置 方便快速开发

--- src/main/java/com/jeeframework/logicframework/util/server/JeeFrameWorkServer.java
+++ src/main/java/com/jeeframework/logicframework/util/server/JeeFrameWorkServer.java
@@ -13,9 +13,12 @@ import com.jeeframework.logicframework.util.server.http.HttpServer;
 import com.jeeframework.logicframework.util.server.tcp.BaseNetController;
 import com.jeeframework.logicframework.util.server.tcp.MinaTcpServer;
 import com.jeeframework.logicframework.util.server.tcp.protocol.ProtocolParser;
+import com.jeeframework.util.format.DateFormat;
 import com.jeeframework.util.properties.JeeProperties;
 import com.jeeframework.util.resource.ResolverUtil;
 import com.jeeframework.util.string.StringUtils;
+import com.jeeframework.util.validate.Validate;
+import org.apache.commons.lang.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
@@ -25,12 +28,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 基础服务类启动类
@@ -72,6 +73,113 @@ public class JeeFrameWorkServer {
 
     public void start() {
         serverProperties = new JeeProperties(SERVER_CONFIG_FILE, false);
+
+
+//        -Dlog.dir=d:\log\guanv
+//                -Dconf.env=local
+//                -Drun.log.additivity=true
+//                -Droot.log.level=debug
+//                -Drun.log.level=debug
+
+        //添加环境变量默认值
+        //日志目录
+        String logDir = null;
+        try {
+            logDir = System.getProperty("log.dir");
+        } catch (Exception e) {
+        }
+        if (Validate.isEmpty(logDir)) {
+
+            String[] dirArrays = null;
+            if (SystemUtils.IS_OS_WINDOWS) {
+                dirArrays = new String[]{"d:", "e:", "f:", "g:", "c:"};
+            } else if (SystemUtils.IS_OS_MAC || SystemUtils.IS_OS_MAC_OSX) {
+
+            } else if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_UNIX) {
+                dirArrays = new String[]{"/tmp/log", "/var/log"};
+            }
+
+            for (String dirDirectoryTmp : dirArrays) {
+
+                logDir = dirDirectoryTmp + File.separator + "log";
+
+                File logDirFile = new File(logDir);
+                //文件不存在就mkdirs
+                if (!logDirFile.exists()) {
+
+                    if (logDirFile.mkdirs()) {
+                        break;
+                        //如果创建目录成功也退出循环，赋值到  log.dir 系统变量里去
+                    }
+                } else {
+                    break;
+                }
+            }
+
+
+            //由于没有指定log目录，需要生成一个临时目录用于区分不同项目，这里按照天yyyymmdd生成目录
+            logDir = logDir + File.separator + DateFormat.formatDate(new Date(), DateFormat.DT_YYYYMMDD);
+            File logDirFile = new File(logDir);
+            //文件不存在就mkdirs
+            if (!logDirFile.exists()) {
+                if (!logDirFile.mkdirs()) {
+                    System.err.println("===============创建日志目录  " + logDir + "   失败，请检查 ==================");
+                    System.exit(0);
+
+                }
+            }
+            System.setProperty("log.dir", logDir);
+            System.out.println("===============没有配置  -Dlog.dir   环境变量，使用默认配置  " + logDir + "  ==================");
+        }
+
+        //配置文件环境变量
+        String confEnv = null;
+        try {
+            confEnv = System.getProperty("conf.env");
+        } catch (Exception e) {
+        }
+        if (Validate.isEmpty(confEnv)) {
+            confEnv = "local";
+            System.setProperty("conf.env", confEnv);
+            System.out.println("===============没有配置  -Dconf.env   环境变量，使用默认配置  " + confEnv + "  ==================");
+        }
+
+        //系统全局日志
+        String rootLogLevel = null;
+        try {
+            rootLogLevel = System.getProperty("root.log.level");
+        } catch (Exception e) {
+        }
+        if (Validate.isEmpty(rootLogLevel)) {
+            rootLogLevel = "debug";
+            System.setProperty("root.log.level", rootLogLevel);
+            System.out.println("===============没有配置  -Droot.log.level   环境变量，使用默认配置  " + rootLogLevel + "  ==================");
+        }
+
+        //模块运行时日志
+        String runLogLevel = null;
+        try {
+            runLogLevel = System.getProperty("run.log.level");
+        } catch (Exception e) {
+        }
+        if (Validate.isEmpty(runLogLevel)) {
+            runLogLevel = "debug";
+            System.setProperty("run.log.level", runLogLevel);
+            System.out.println("===============没有配置  -Drun.log.level   环境变量，使用默认配置  " + runLogLevel + "  ==================");
+        }
+
+        //是否追加到rootlog中
+        boolean runLogAdditivity = true;
+        try {
+            runLogAdditivity = Boolean.valueOf(System.getProperty("run.log.additivity"));
+        } catch (Exception e) {
+            runLogAdditivity = true;
+        }
+        if (runLogAdditivity) {
+            System.setProperty("run.log.additivity", "true");
+        } else {
+            System.setProperty("run.log.additivity", "false");
+        }
 
         httpServer = new HttpServer();
         httpServer.start();
